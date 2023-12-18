3f360ed1eac3d43a0db44477caa403de4d2c150e
Kaio Gomes <kaiogomes.kor@gmail.com>
2023-12-18 18:43:45 -0300

    Finalizando App de Comida

--- pgtestes.py
+++ pgtestes.py
@@ -1,19 +1,4 @@
-class Cliente:
-    def __init__(self,nome):
-        # para o property funcionar, temos que deixar o nosso atributo privado, se não, vai dar erro
-        self.__nome = nome
+a = False
 
-    @property
-    def nom(self):
-        print("chamando @property nome()")
-                        # title deixa a primeira letra maiúscula
-        return self.__nome.title()
-    
-    @nom.setter
-    def nome(self,nome):
-        print("chamando setter nome()")
-        self.__nome = nome
-
-cliente1 = Cliente('samara')
-
-print(cliente1.nom)
+if a == False:
+    print(a)
